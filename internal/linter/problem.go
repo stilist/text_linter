625d6ffea31a177ed1e2a7b14b294af6171e8158
Jordan Cole <33663+stilist@users.noreply.github.com>
2020-04-17 15:02:05 -0700

    Work on improving Problem#Describe output

--- internal/linter/problem.go
+++ internal/linter/problem.go
@@ -11,11 +11,28 @@ type Problem struct {
 	Replacement string
 }
 
+const samMaxLen = 80
+const (
+	snipBef = "[…] "
+	snipAft = " […]"
+)
+
 func (p *Problem) Describe() {
-	fmt.Printf("%s: %s @[%d,%d):\n-> %s\n",
+	sam := p.Text[p.Position.Start:p.Position.End]
+	samLen := len(sam)
+	if samLen < samMaxLen {
+		if len(p.Text) > p.Position.End && samLen+len(snipAft) <= samMaxLen {
+			sam = sam + snipAft
+		}
+		if p.Position.Start > 0 && samLen+len(snipBef) <= samMaxLen {
+			sam = snipBef + sam
+		}
+	}
+
+	fmt.Printf("%s: %s @[%d,%d):\n-> %s\n\n",
 		p.RuleMetadata.ID,
-		p.RuleMetadata.Description,
+		p.RuleMetadata.Name,
 		p.Position.Start,
 		p.Position.End,
-		p.Text[p.Position.Start:p.Position.End])
+		sam)
 }
