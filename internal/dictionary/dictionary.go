81e728dbd489407c9ef34e7a4894bc3fa948ea3b
Jordan Cole <33663+stilist@users.noreply.github.com>
2020-04-28 18:57:12 -0700

    Overhaul dictionary CSV format to match STE documentation
    The code converts the data into the same format as before, but this 
    makes it simpler to reuse the data from the CSV. Most of the cells are 
    currently empty, because I prioritized entering the words over entering 
    the complete data set.

--- internal/dictionary/dictionary.go
+++ internal/dictionary/dictionary.go
@@ -3,16 +3,19 @@ package dictionary
 import (
 	"bufio"
 	"encoding/csv"
+	"fmt"
 	"io"
+	"log"
+	"regexp"
 	"strings"
 )
 
 type Entry struct {
-	Approved bool
-	Text     string
-	// @see https://www.ling.upenn.edu/courses/Fall_2003/ling001/penn_treebank_pos.html
+	Approved         bool
+	Text             string
 	Tag              string
 	Meaning          string
+	Note             string
 	Alternatives     []string
 	Examples         []string
 	NegativeExamples []string
@@ -24,27 +27,145 @@ func (e *Entry) CheckTag(t string) bool {
 
 type Dictionary map[string]Entry
 
-var csvHeader = []string{
-	"keyword",
-	"tag",
-	"meaning",
-	"alternatives",
-	"example",
-	"negative_example",
+// @see https://www.ling.upenn.edu/courses/Fall_2003/ling001/penn_treebank_pos.html
+var posLookup = map[string]string{
+	"adj": "JJ",
+	"adv": "RB",
+	"art": "DT",
+	// CC for coordinating conjunction
+	// IN subordinating conjunction
+	"con": "CC",
+	"n":   "NN",
+	// PRP for personal pronoun
+	// PRP$ for possessive pronoun
+	// WP for wh-pronoun
+	// WP$ for possessive wh-pronoun
+	"pn":  "PRP",
+	"pre": "IN",
+	"v":   "VB",
+}
+
+func entriesFromRow(row []string) ([]Entry, error) {
+	es := []Entry{}
+
+	keyword := row[0]
+	var pos_tags []string
+	for pos := range posLookup {
+		pos_tags = append(pos_tags, pos)
+	}
+	pos_re := regexp.MustCompile(`\s+\((` + strings.Join(pos_tags, "|") + `)\)`)
+	var pos string
+	if matches := pos_re.FindStringSubmatch(keyword); matches != nil {
+		pos = matches[1]
+		keyword = strings.Replace(keyword, matches[0], "", 1)
+	} else {
+		fmt.Printf("Didn't find a PoS tag in '%s'.", keyword)
+	}
+
+	var note string
+	note_re := regexp.MustCompile(`\s+NOTE:\s+(.+)`)
+	if matches := note_re.FindStringSubmatch(keyword); matches != nil {
+		note = matches[1]
+		keyword = strings.Replace(keyword, matches[0], "", 1)
+	}
+
+	l1 := string(keyword[0])
+	approved := l1 == strings.ToUpper(l1)
+
+	// This variable has a generic name because the column's data serves two
+	// incompatible purposes: for approved words it lists the approved
+	// definition; for unapproved words it lists approved alternative words.
+	col2 := row[1]
+	// Either of the first two columns may have a NOTE, but there won't be a
+	// NOTE in *both* columns.
+	if note == "" {
+		if matches := note_re.FindStringSubmatch(col2); matches != nil {
+			note = matches[1]
+			col2 = strings.Replace(col2, matches[0], "", 1)
+		}
+	}
+	var meaning string
+	var alternatives []string
+	if approved {
+		meaning = col2
+	} else {
+		alternatives = strings.Split(strings.ToLower(col2), ",")
+	}
+
+	examples := strings.Split(row[2], "\n\n")
+	negative_examples := strings.Split(row[3], "\n\n")
+
+	var ks_re *regexp.Regexp
+	// This regex strips out cross-referenced words.
+	//
+	// @example
+	//   "BE, IS, WAS (also ARE, WERE)" will match "BE, IS, WAS"
+	// @example
+	//   "BAD (adj) (WORSE, WORST)" will match "BAD" (WORSE/WORST are listed
+	//   separately)
+	if approved {
+		ks_re = regexp.MustCompile(`^[A-Z,\s]+`)
+	} else {
+		ks_re = regexp.MustCompile(`^[a-z,\s]+`)
+	}
+	all_ks := ks_re.FindString(keyword)
+	ks := strings.Split(all_ks, ", ")
+	for i, k := range ks {
+		tag := posLookup[pos]
+		// Some verbs will have a list of three or four approved tenses. This
+		// tags the additional tenses.
+		//
+		// @example
+		//   BECOME, BECOMES, BECAME
+		//   BLOW, BLOWS, BLEW, BLOWN
+		if pos == "v" && i > 0 {
+			if i == 1 {
+				// BECOMES, BLOWS: Verb, gerund or present participle
+				tag = "VBG"
+			} else if i == len(ks)-1 {
+				// BECAME, BLOWN: Verb, past tense
+				tag = "VBD"
+			} else {
+				// BLEW: Verb, past participle
+				tag = "VBN"
+			}
+		}
+
+		text := strings.Trim(strings.ToLower(k), " ")
+		e := Entry{
+			Approved:         approved,
+			Text:             text,
+			Note:             note,
+			Tag:              tag,
+			Alternatives:     alternatives,
+			Meaning:          meaning,
+			Examples:         examples,
+			NegativeExamples: negative_examples,
+		}
+		es = append(es, e)
+	}
+
+	return es, nil
+}
+
+var CSVColumns = []string{
+	"Keyword",
+	"Approved meaning/ALTERNATIVES",
+	"APPROVED EXAMPLE",
+	"Not approved",
 }
 
 func FromCSV(r io.Reader) (Dictionary, error) {
 	d := Dictionary{}
-
 	// skip header row
 	br := bufio.NewReader(r)
 	_, err := br.ReadString('\n')
 	if err != nil {
-		return nil, err
+		return d, err
 	}
 
 	cr := csv.NewReader(br)
-	cr.FieldsPerRecord = len(csvHeader)
+	cr.FieldsPerRecord = len(CSVColumns)
 	for {
 		row, err := cr.Read()
 		if err == io.EOF {
@@ -53,18 +174,13 @@ func FromCSV(r io.Reader) (Dictionary, error) {
 			return nil, err
 		}
 
-		text := row[0]
-		e := Entry{
-			Approved:         row[2] != "",
-			Text:             text,
-			Tag:              row[1],
-			Alternatives:     strings.Split(row[3], ","),
-			Meaning:          row[2],
-			Examples:         strings.Split(row[4], ". "),
-			NegativeExamples: strings.Split(row[5], ". "),
+		es, err := entriesFromRow(row)
+		if err != nil {
+			log.Fatal(err)
 		}
-
-		d[text] = e
+		for _, e := range es {
+			d[e.Text] = e
+		}
 	}
 
 	return d, nil
